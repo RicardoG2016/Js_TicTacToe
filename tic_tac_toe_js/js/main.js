04309edd3276fc1ca46b8fbed12c8afc22cd49fa
RicardoG2016 <gomez.r@live.com>
2017-01-09 14:40:38 -0600

    refactored code to consolidate it and reduce lines

--- tic_tac_toe_js/js/main.js
+++ tic_tac_toe_js/js/main.js
@@ -2,29 +2,15 @@ $(document).ready(function(){
   console.log("hey");
 });
 
-
-
 var button = [];
-
-for (var i =1; i < 10; i++)
-  {
-    button[i] = document.getElementById('canvas'+i);
-  }
+for (var i =1; i < 10; i++) button[i] = document.getElementById('canvas'+i);
 button[i]
 
 var ctx = [];
-
-for (var i =1; i < 10; i++)
-  {
-    ctx[i] = button[i].getContext('2d');
-  }
+for (var i =1; i < 10; i++) ctx[i] = button[i].getContext('2d');
 
 var bDisabled = [];
-
-for (var i =1; i < 10; i++)
-  {
-    bDisabled[i] = false;
-  }
+for (var i =1; i < 10; i++) bDisabled[i] = false;
 
 var isResult = false;
 var content = [];
