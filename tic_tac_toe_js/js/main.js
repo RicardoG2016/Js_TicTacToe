66043ad75e678784ab2843f6dbc1d471375c0cc5
RicardoG2016 <gomez.r@live.com>
2017-01-09 14:09:30 -0600

    added game logic to js file that will hold values of game and run loop to check canvas cells to see if there is a value in each one to store

--- tic_tac_toe_js/js/main.js
+++ tic_tac_toe_js/js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
   console.log("hey");
 });
+
+
+
+var button = [];
+
+for (var i =1; i < 10; i++)
+  {
+    button[i] = document.getElementById('canvas'+i);
+  }
+button[i]
+
+var ctx = [];
+
+for (var i =1; i < 10; i++)
+  {
+    ctx[i] = button[i].getContext('2d');
+  }
+
+var bDisabled = [];
+
+for (var i =1; i < 10; i++)
+  {
+    bDisabled[i] = false;
+  }
+
+var isResult = false;
+var content = [];
