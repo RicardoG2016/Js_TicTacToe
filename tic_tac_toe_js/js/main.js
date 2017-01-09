5c1242a1ae6f4379dc1f60cb73aa74c20da5b9a9
RicardoG2016 <gomez.r@live.com>
2017-01-09 15:21:13 -0600

    added animations and additional code for it to work on all browsers.

--- tic_tac_toe_js/js/main.js
+++ tic_tac_toe_js/js/main.js
@@ -1,16 +1,30 @@
-$(document).ready(function(){
-  console.log("hey");
-});
-
 var button = [];
-for (var i =1; i < 10; i++) button[i] = document.getElementById('canvas'+i);
-button[i]
+for (var i = 1; i < 10; i++) button[i] = document.getElementById('canvas'+i);
 
 var ctx = [];
-for (var i =1; i < 10; i++) ctx[i] = button[i].getContext('2d');
+for (var i = 1; i < 10; i++) ctx[i] = button[i].getContext('2d');
 
 var bDisabled = [];
-for (var i =1; i < 10; i++) bDisabled[i] = false;
+for (var i = 1; i < 10; i++) bDisabled[i] = false;
 
 var isResult = false;
 var content = [];
+
+// x = the clicked canvas button.
+function loop(x)
+{
+  if(bDisabled[x] == false)
+  {
+    bDisabled[x] = true;
+    button[x].style.opacity = 0.7;
+// content[x] is used to decide winner with winner function.
+    content[x] = 'x';
+// rotateY created animation when button is clicked
+    button[x].style.Transform = "rotateY(180deg)"; 
+    button[x].style.webkitTransform = "rotateY(180deg)"; 
+    button[x].style.msTransform = "rotateY(180deg)"; 
+    button[x].style.mozTransform = "rotateY(180deg)"; 
+    button[x].style.oTransform = "rotateY(180deg)"; 
+  }
+
+}
